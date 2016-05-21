55af164dc4bfe95b944d217ca36c2638f60f15ee
cDor <christoph.dornieden@gmail.com>
2016-05-21 18:30:50 +0200

    sth

--- player.js
+++ player.js
@@ -4,10 +4,14 @@ module.exports = {
   VERSION: "Default JavaScript folding player",
 
   bet_request: function(game_state, bet) {
-    bet(150);
+    //Check for first
+    console.log(game_state);
+    bet(game_state.minimum_raise);
   },
-
   showdown: function(game_state) {
-
   }
 };
+
+function first (game_state, bet){
+
+}
