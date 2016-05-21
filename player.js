bc9d7ce3491b04ba25f6269d738634952f8d03a4
cDor <christoph.dornieden@gmail.com>
2016-05-21 18:51:07 +0200

    call changed

--- player.js
+++ player.js
@@ -1,33 +1,40 @@
+let gameState, respond;
 module.exports = {
   VERSION: "Kickstarter",
 
   bet_request: function(game_state, bet) {
+    gameState =  game_state;
+    respond = bet;
     const myplayer = game_state.players[game_state.in_action]
     console.log(myplayer.hold_cards)
     //Check for first
     console.log(game_state);
-    call(game_state, bet);
+    call();
   },
   showdown: function(game_state) {
   }
 };
 
 function phase_zero (game_state, bet){
+  call();
 }
 
 function phase_three (game_state, bet){
+  call();
 
 }
 
 function phase_four (game_state, bet){
+  call();
 
 }
 
 function phase_five (game_state, bet){
+  call();
 
 }
 
-function call (game_state, bet){
-  const c = game_state.current_buy_in - game_state.players[game_state.in_action].bet;
-  bet(c);
+function call (){
+  const c = gameState.current_buy_in - gameState.players[game_state.in_action].bet;
+  respond(c);
 }
