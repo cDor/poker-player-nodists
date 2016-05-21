bbd55d2032632bb1ccf470472fd7b1b2d0b3188a
cDor <christoph.dornieden@gmail.com>
2016-05-21 18:37:27 +0200

    more logging

--- player.js
+++ player.js
@@ -4,6 +4,8 @@ module.exports = {
   VERSION: "Kickstarter",
 
   bet_request: function(game_state, bet) {
+    const myplayer = game_state.players[game_state.in_action]
+    console.log(myplayer.hold_cards)
     //Check for first
     console.log(game_state);
     bet(game_state.minimum_raise);
@@ -12,6 +14,19 @@ module.exports = {
   }
 };
 
-function first (game_state, bet){
+function zero (game_state, bet){
+  if()
+
+}
+
+function three (game_state, bet){
+
+}
+
+function four (game_state, bet){
+
+}
+
+function five (game_state, bet){
 
 }
