bbd5c8197a2cd04ac2c618647eb2fec8d7534312
Mathis Neumann <mathis@simpletechs.net>
2016-05-21 19:36:49 +0200

    promise based arch

--- player.js
+++ player.js
@@ -1,50 +1,55 @@
 'use strict';
 
-let gameState, respond;
+let gameState;
 module.exports = {
   VERSION: "Kickstarter",
 
   bet_request: function(game_state, bet) {
-    gameState =  game_state;
-    respond = bet;
-    const myplayer = game_state.players[game_state.in_action];
-    console.log(myplayer.hole_cards);
-    console.log(game_state);
-    //Check for zero phase
-    if(gameState.community_cards.length == 0){
-      phase_zero();
-    }else if(gameState.community_cards.length == 3){
-      phase_three();
-    }else if(gameState.community_cards.length == 4){
-      phase_four();
-    }else if(gameState.community_cards.length == 5){
-      phase_five();
-    }
+    new Promise(resolve => {
+        gameState =  game_state;
+        const myplayer = game_state.players[game_state.in_action];
+        console.log(myplayer.hole_cards);
+        console.log(game_state);
+
+
+        //Check for zero phase
+        if(gameState.community_cards.length === 0){
+          resolve(phase_zero());
+        }else if(gameState.community_cards.length === 3){
+          resolve(phase_three());
+        }else if(gameState.community_cards.length === 4){
+          resolve(phase_four());
+        }else {
+          resolve(phase_five());
+        }
+    }).then(value => {
+        bet(value);
+    }).catch(err => {
+        console.log('ERROR', err);
+    });
   },
   showdown: function(game_state) {
   }
 };
 
-function phase_zero (game_state, bet){
+function phase_zero (){
   console.log("phase zero");
-  call();
+  return call();
 }
 
-function phase_three (game_state, bet){
-  call();
-
+function phase_three (){
+  return call();
 }
 
 function phase_four (game_state, bet){
-  call();
-
+  return call();
 }
 function phase_five (game_state, bet){
-  call();
+  return call();
 
 }
 
 function call (){
   const c = gameState.current_buy_in - gameState.players[gameState.in_action].bet;
-  respond(c);
+  return c;
 }
