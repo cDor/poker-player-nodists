521a3c6b050026c9155738bbdc3d865ae12ef886
Mathis Neumann <mathis@simpletechs.net>
2016-05-21 21:16:23 +0200

    fix

--- ranking.js
+++ ranking.js
@@ -3,14 +3,16 @@
 const request = require('request-promise');
 
 module.exports = function(cards) {
-    request({
+    return request({
         uri: 'http://rainman.leanpoker.org/rank?cards='+encodeURIComponent(JSON.stringify(cards)),
         timeout: 2000
     })
     .then(function (answer) {
         console.log('answer', answer);
+        return answer;
     })
     .catch(function (err) {
         console.error('error', err);
+        throw err;
     });
 };
